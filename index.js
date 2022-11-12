864a2510b7320b6af7d076486efc655197745634
Alexandr An <alexandr13an@yandex.ru>
2022-11-12 07:11:53 +0300

    client dist path fixed 2

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ app.use(express.json());
 app.use(cors());
 app.use('/', router);
 
-app.use(express.static(path.join(path.dirname(__dirname), 'client/dist')))
+app.use(express.static(path.join(path.dirname(__dirname), 'qr-scanner-server/client/dist')))
 
 app.use(errorHandler);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(path.dirname(__dirname), 'client/dist/index.html'))
+  res.sendFile(path.join(path.dirname(__dirname), 'qr-scanner-server/client/dist/index.html'))
 })
 
 const httpsOptions = {
