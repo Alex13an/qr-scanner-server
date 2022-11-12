e4a3c3d17aab8ecf1429973ad212d96f8883db2d
Alexandr An <alexandr13an@yandex.ru>
2022-11-12 06:27:16 +0300

    moved to http

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import knex from './db/knex.js';
 import errorHandler from './middleware/ErrorHandler.js';
 import GoogleSheetsRepository from './repositories/GoogleSheetsRepository.js';
 import GoogleSheetsUpdater from './repositories/GoogleSheetsUpdaterRepository.js';
-import https from 'https';
+import http from 'http';
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,14 +16,14 @@ app.use(cors());
 app.use('/', router);
 app.use(errorHandler);
 
-const httpsServer = https.createServer(app);
+const httpServer = http.createServer(app);
 
 (async () => {
   try {
     await knex.migrate.latest();
     console.log('DB loaded successfully');
     await GoogleSheetsRepository.getUpdates();
-    httpsServer.listen(PORT, () => console.log(`server started on port ${PORT}`));
+    httpServer.listen(PORT, () => console.log(`server started on port ${PORT}`));
     GoogleSheetsUpdater.startTimer();
   } catch (err) {
     console.log(err);
